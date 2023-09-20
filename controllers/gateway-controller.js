59e5abb45820ea40d6cf6afc0e98daafc428d654
kacm79 <105650651+kacm79@users.noreply.github.com>
2023-09-20 16:18:53 +0100

    added handling for username taken

--- controllers/gateway-controller.js
+++ controllers/gateway-controller.js
@@ -1,15 +1,36 @@
-import {User} from "../models/user.js"
-import { v4 as uuid } from "uuid"
+import {User} from "../models/user.js";
+import { v4 as uuid } from "uuid";
 
 export const createUser = (req, res) => {
-    const {username, password, organisation, yearGroup} = req.body
-    const user = new User({username, password, organisation, yearGroup, token: uuid()})
-    user.save().then((savedUser) => {
-        res.send(savedUser)
-    }).catch((error) => {
-        res.status(500).json({message: "Sorry, something's gone wrong"})
-    })
-}
+    const { username, password, organisation, yearGroup } = req.body;
+
+    User.findOne({ username })
+        .then((existingUser) => {
+            if (existingUser) {
+                return res.status(400).json({ message: "Username already taken" });
+            }
+
+            const user = new User({
+                username,
+                password,
+                organisation,
+                yearGroup,
+                token: uuid(),
+            });
+
+            user
+                .save()
+                .then((savedUser) => {
+                    res.send(savedUser);
+                })
+                .catch((error) => {
+                    res.status(500).json({ message: "Sorry, something's gone wrong" });
+                });
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Sorry, something's gone wrong" });
+        });
+};
 
 export const login = (req, res) => {
 
@@ -17,14 +38,14 @@ export const login = (req, res) => {
 
     User.findOne({ username }).then((user) => {
         if (!user) {
-            return res.status(404).json({ message: "Username not found" })
+            return res.status(404).json({ message: "Username not found" });
         }
         if (user.password !== password) {
-            return res.status(401).json({ message: "Incorrect password" })
+            return res.status(401).json({ message: "Incorrect password" });
         }
         user.token = uuid()
         res.send(user)
     }).catch((error) => {
-        res.status(500).json({ message: "Sorry, something's gone wrong" })
+        res.status(500).json({ message: "Sorry, something's gone wrong" });
     })
 }
--- controllers/generate-controller.js
+++ controllers/generate-controller.js
@@ -22,7 +22,7 @@ export const generateReport = async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt,
-            temperature: 1,
+            temperature: 0.8,
             max_tokens: 1000,
             top_p: 1,
             frequency_penalty: 0,
